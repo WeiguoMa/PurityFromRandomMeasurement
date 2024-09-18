0ab430b40ebeeae9f41bb8e97f65a7c5c76925a1
WeiguoMA <david.mawg@gmail.com>
2024-09-18 19:49:26 +0800

    Formatted into .h file for indexing.

--- src/cpp/ShadowState.cpp
+++ src/cpp/ShadowState.cpp
@@ -1,102 +1,91 @@
 //
 // Created by Weiguo Ma on 2024/9/16.
 //
-#include <Eigen/Dense>
-#include <cmath>
-#include <complex>
-#include <vector>
-#include <map>
-#include <string>
+#include "ShadowState.h"
 
-using namespace Eigen;
-using namespace std;
 
-typedef complex<double> cd;
+std::size_t matrix_hash::operator()(const std::pair<int, int> &p) const {
+    std::size_t seed = 0;
+    seed ^= std::hash<int>{}(p.first) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    seed ^= std::hash<int>{}(p.second) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    return seed;
+}
 
-struct matrix_hash {
-    std::size_t operator()(const std::pair<int, int> &p) const {
-        std::size_t seed = 0;
-        seed ^= std::hash<int>{}(p.first) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-        seed ^= std::hash<int>{}(p.second) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-        return seed;
-    }
-};
 
-class ShadowState {
-private:
-    static const vector<MatrixXcd> _bases;
-    static const vector<MatrixXcd> _pauliBases;
-    unordered_map<pair<int, int>, MatrixXcd, matrix_hash> precomputedResults;
+ShadowState::ShadowState() {
+    precomputeAll();
+}
 
-    void precomputeAll() {
-        for (int i = 0; i < _pauliBases.size(); ++i) {
-            for (int j = 0; j < _bases.size(); ++j) {
-                MatrixXcd ketResult = _pauliBases[i].adjoint() * _bases[j];
-                MatrixXcd rhoResult = ketResult * ketResult.adjoint();
-                precomputedResults[{i, j}] = rhoResult;
-            }
+
+void ShadowState::precomputeAll() {
+    for (int i = 0; i < _pauliBases.size(); ++i) {
+        for (int j = 0; j < _bases.size(); ++j) {
+            MatrixXcd ketResult = _pauliBases[i].adjoint() * _bases[j];
+            MatrixXcd rhoResult = ketResult * ketResult.adjoint();
+            precomputedResults[{i, j}] = rhoResult;
         }
     }
+}
 
-    template<typename MatrixType>
-    static MatrixType kroneckerProduct(const MatrixType &A, const MatrixType &B) {
-        const int rowsA = A.rows();
-        const int colsA = A.cols();
-        const int rowsB = B.rows();
-        const int colsB = B.cols();
 
-        MatrixType result(rowsA * rowsB, colsA * colsB);
+template<typename MatrixType>
+MatrixType ShadowState::kroneckerProduct(const MatrixType &A, const MatrixType &B) {
+    const int rowsA = A.rows();
+    const int colsA = A.cols();
+    const int rowsB = B.rows();
+    const int colsB = B.cols();
 
-        #pragma omp parallel for collapse(2)
-        for (int i = 0; i < rowsA; ++i) {
-            for (int j = 0; j < colsA; ++j) {
-                result.block(i * rowsB, j * colsB, rowsB, colsB) = A(i, j) * B;
-            }
+    MatrixType result(rowsA * rowsB, colsA * colsB);
+
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < rowsA; ++i) {
+        for (int j = 0; j < colsA; ++j) {
+            result.block(i * rowsB, j * colsB, rowsB, colsB) = A(i, j) * B;
         }
-        return result;
     }
+    return result;
+}
 
-    template<typename MatrixType>
-    static MatrixType kroneckerProduct(const std::vector<MatrixType> &matrixList) {
-        MatrixType result = matrixList[0];
-        for (size_t i = 1; i < matrixList.size(); ++i) {
-            result = kroneckerProduct(result, matrixList[i]);
-        }
-        return result;
+
+template<typename MatrixType>
+MatrixType ShadowState::kroneckerProduct(const std::vector<MatrixType> &matrixList) {
+    MatrixType result = matrixList[0];
+    for (size_t i = 1; i < matrixList.size(); ++i) {
+        result = kroneckerProduct(result, matrixList[i]);
     }
+    return result;
+}
 
-public:
-    MatrixXcd measureResult2state(
-            const vector<int> &measureOperation,
-            const vector<int> &measureResult
-    ) {
-        vector<MatrixXcd> _state;
-        _state.reserve(measureOperation.size());
 
-        for (size_t idx = 0; idx < measureOperation.size(); ++idx) {
-            _state.emplace_back(
-                    3 * precomputedResults[{measureOperation[idx], measureResult[idx]}] - Matrix2cd::Identity()
-            );
-        }
-        return kroneckerProduct(_state);
+MatrixXcd ShadowState::measureResult2state(
+        const vector<int> &measureOperation,
+        const vector<int> &measureResult
+) {
+    vector<MatrixXcd> _state;
+    _state.reserve(measureOperation.size());
+
+    for (size_t idx = 0; idx < measureOperation.size(); ++idx) {
+        _state.emplace_back(
+                3 * precomputedResults[{measureOperation[idx], measureResult[idx]}] - Matrix2cd::Identity()
+        );
     }
+    return kroneckerProduct(_state);
+}
 
-    MatrixXcd stateEstimation(
-            const vector<vector<int>> &measureOperations,
-            const vector<vector<int>> &measureResults
-    ) {
-        MatrixXcd sumMatrix = MatrixXcd::Zero(precomputedResults.begin()->second.rows(),
-                                              precomputedResults.begin()->second.cols());
-        for (size_t idx = 0; idx < measureOperations.size(); ++idx) {
-            sumMatrix += measureResult2state(measureOperations[idx], measureResults[idx]);
-        }
-        return sumMatrix / sumMatrix.size();
-    }
 
-    ShadowState() {
-        precomputeAll();
+MatrixXcd ShadowState::stateEstimation(
+        const vector<vector<int>> &measureOperations,
+        const vector<vector<int>> &measureResults
+) {
+    // hilbert_dimension = 2^N, N is the number of qubits
+    int hilbert_dimension = static_cast<int>(pow(2, static_cast<int>(measureOperations.size())));
+    MatrixXcd sumMatrix = MatrixXcd::Zero(hilbert_dimension, hilbert_dimension);
+    for (size_t idx = 0; idx < measureOperations.size(); ++idx) {
+        sumMatrix += ShadowState::measureResult2state(measureOperations[idx], measureResults[idx]);
     }
-};
+    return sumMatrix / sumMatrix.size();
+}
+
 
 const vector<MatrixXcd> ShadowState::_pauliBases = {
         (MatrixXcd(2, 2) << 1, 1, -1, 1).finished() / sqrt(2.0),
---
+++ src/cpp/ShadowState.h
@@ -0,0 +1,43 @@
+//
+// Created by Weiguo Ma on 2024/9/18.
+//
+
+#ifndef PURITYFROMSHADOW_SHADOWSTATE_H
+#define PURITYFROMSHADOW_SHADOWSTATE_H
+
+#include <Eigen/Dense>
+#include <complex>
+#include <vector>
+#include <unordered_map>
+#include <utility>
+
+using namespace Eigen;
+using namespace std;
+
+typedef complex<double> cd;
+
+struct matrix_hash {
+    std::size_t operator()(const std::pair<int, int> &p) const;
+};
+
+class ShadowState {
+private:
+    static const vector<MatrixXcd> _bases;
+    static const vector<MatrixXcd> _pauliBases;
+    unordered_map<pair<int, int>, MatrixXcd, matrix_hash> precomputedResults;
+
+    void precomputeAll();
+
+    template<typename MatrixType>
+    static MatrixType kroneckerProduct(const MatrixType &A, const MatrixType &B);
+
+    template<typename MatrixType>
+    static MatrixType kroneckerProduct(const std::vector<MatrixType> &matrixList);
+
+public:
+    ShadowState();
+    MatrixXcd measureResult2state(const vector<int> &measureOperation, const vector<int> &measureResult);
+    MatrixXcd stateEstimation(const vector<vector<int>> &measureOperations, const vector<vector<int>> &measureResults);
+};
+
+#endif //PURITYFROMSHADOW_SHADOWSTATE_H
