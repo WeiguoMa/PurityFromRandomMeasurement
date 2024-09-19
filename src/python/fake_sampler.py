ae278cf43ed8799ca9ec8d5b539e3ed652612e3d
WeiguoMA <david.mawg@gmail.com>
2024-09-19 17:07:15 +0800

    Fake Sampler

--- src/python/fake_sampler.py
+++ src/python/fake_sampler.py
@@ -6,7 +6,7 @@ from typing import Union, List, Optional
 import numpy as np
 from qutip import Qobj
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../build')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../dependency')))
 
 
 class FakeSampler:
@@ -24,12 +24,14 @@ class FakeSampler:
         self.backend = FakeSampler_backend(proj_basis=self.basis)
 
     def fake_sampling_dm(self, dm: Union[np.ndarray, Qobj],
-                         measurement_orientation: Optional[List[str]] = None):
+                         measure_times: int,
+                         measurement_orientation: Optional[List[int]] = None) -> List[List[int]]:
         """
         Perform fake sampling on a density matrix.
 
         Args:
             dm (Union[np.ndarray, Qobj]): The density matrix.
+            measure_times: K times of measurement.
             measurement_orientation (Optional[List[str]]): The measurement orientations.
 
         Returns:
@@ -47,10 +49,26 @@ class FakeSampler:
             raise TypeError("The type of Density Matrix must be np.ndarray or qutip.Qobj.")
 
         if measurement_orientation is None:
-            measurement_orientation = ['Z'] * system_size
+            measurement_orientation = [2] * system_size
         elif len(measurement_orientation) != system_size:
             raise ValueError("The length of measurement_orientation must be equal to the system size.")
 
-        state01 = self.backend.fakeSampling_dm(dm_array=dm, measurement_orientation=measurement_orientation)
+        state01 = self.backend.fakeSampling_dm(dm_array=dm,
+                                               measure_times=measure_times,
+                                               measurement_orientation=measurement_orientation)
 
-        return measurement_orientation, state01
+        return state01
+
+
+def random_measurementScheme(qnumber: int, amount: int) -> List[List[int]]:
+    """
+    Generate random measurement scheme for a given qnumber.
+
+    Args:
+        qnumber: The number of qubits.
+        amount: M. The amount of measurement schemes to generate
+
+    Returns:
+        List[str]: The measurement orientations.
+    """
+    return [np.random.choice([0, 1, 2], qnumber).tolist() for _ in range(amount)]
