68d4dc37469ee7cf13b4238affcbfa2734521bf1
WeiguoMA <david.mawg@gmail.com>
2024-09-20 19:35:09 +0800

    add platform dependency

---
+++ src/cpp/platformConfig.h
@@ -0,0 +1,24 @@
+//
+// Created by Weiguo Ma on 2024/9/20.
+//
+
+#ifndef PURITYFROMSHADOW_PLATFORMCONFIG_H
+#define PURITYFROMSHADOW_PLATFORMCONFIG_H
+
+#if defined(_WIN32) || defined(_WIN64)
+    #include <bit>
+    using LoopIndexType = int;
+    using randomGenerator = std::mt19937;
+    inline int countBits(uint64_t x) {
+        return std::popcount(x);
+    }
+#else
+    #include "pcg/pcg_random.hpp"
+    using LoopIndexType = size_t;
+    using randomGenerator = pcg32;
+    inline int countBits(uint64_t x) {
+        return __builtin_popcountll(x);
+    }
+#endif
+
+#endif //PURITYFROMSHADOW_PLATFORMCONFIG_H
