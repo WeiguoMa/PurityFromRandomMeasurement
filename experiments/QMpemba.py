db24c9d3764154a68290de518b50c589e1d3ba92
WeiguoMA <david.mawg@gmail.com>
2024-10-29 20:56:07 +0800

    Data Analysis

--- experiments/QMpemba.py
+++ experiments/QMpemba.py
@@ -1,12 +1,16 @@
 import os
-import sys
-import numpy as np
-from typing import List, Union, Optional, Dict
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
-from qutip_qip.operations import ry
 import pickle
+import sys
+from typing import List, Union, Optional, Dict
+
+import matplotlib.lines as mlines
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import Normalize
+from matplotlib.lines import Line2D
+from matplotlib.patches import Polygon
+from qutip_qip.operations import ry
+from tqdm import tqdm
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
@@ -105,7 +109,7 @@ class EntanglementAsymmetry:
             epoches: Number of epoches to run.
         """
         _results_thetas_epoches_times = np.array([self.slicesRun(rho, M, K, epoches) for rho in rhos])
-        return _results_thetas_epoches_times.transpose(1, 0, 2, 3)      # result_epoches_thetas_times
+        return _results_thetas_epoches_times.transpose(1, 0, 2, 3)  # result_epoches_thetas_times
 
 
 class SimpleModel:
@@ -154,7 +158,7 @@ class SimpleModel:
         return [
             1 / np.sqrt(2) *
             (tensor([ry(theta) * basis(2, 0) for _ in range(self.qNumber)])
-                - tensor([ry(-theta) * basis(2, 0)for _ in range(self.qNumber)]))
+             - tensor([ry(-theta) * basis(2, 0) for _ in range(self.qNumber)]))
             for theta in thetas
         ]
 
@@ -196,7 +200,7 @@ class SimpleModel:
                 initial_states]
 
 
-def data_preparation(results: np.ndarray, **kwargs) -> Dict:
+def data_preparation_EA(results: np.ndarray, **kwargs) -> Dict:
     """
     Args:
         results: Shape - (epoches, thetas, times, 6);
@@ -206,25 +210,116 @@ def data_preparation(results: np.ndarray, **kwargs) -> Dict:
     }
     epoches = results.shape[0]
     renyi_rhoA_ideal, renyi_rhoA_hamming, renyi_rhoA_CS = results[:, :, :, 0], results[:, :, :, 1], results[:, :, :, 2]
-    renyi_rhoAQ_ideal, renyi_rhoAQ_hamming, renyi_rhoAQ_CS = results[:, :, :, 3], results[:, :, :, 4], results[:, :, :, 5]
+    renyi_rhoAQ_ideal, renyi_rhoAQ_hamming, renyi_rhoAQ_CS = results[:, :, :, 3], results[:, :, :, 4], results[:, :, :,
+                                                                                                       5]
 
     _RESULT['AQ_A_IDEAL'] = np.mean(renyi_rhoAQ_ideal - renyi_rhoA_ideal, axis=0)
 
     _RESULT['AQ_A_HAMMING'] = np.mean(renyi_rhoAQ_hamming - renyi_rhoA_hamming, axis=0)
     _RESULT['AQ_A_CS'] = np.mean(renyi_rhoAQ_CS - renyi_rhoA_CS, axis=0)
-    _RESULT['AQ_A_HAMMING_ERRORBAR'] = np.std(np.abs(renyi_rhoAQ_hamming - renyi_rhoA_hamming), axis=0) / np.sqrt(epoches)
+
+    _RESULT['AQ_A_HAMMING_ERRORBAR'] = np.std(np.abs(renyi_rhoAQ_hamming - renyi_rhoA_hamming), axis=0) / np.sqrt(
+        epoches)
     _RESULT['AQ_A_CS_ERRORBAR'] = np.std(np.abs(renyi_rhoAQ_CS - renyi_rhoA_CS), axis=0) / np.sqrt(epoches)
 
+    _RESULT['A_IDEAL'] = np.mean(renyi_rhoA_ideal, axis=0)
+    _RESULT['AQ_IDEAL'] = np.mean(renyi_rhoAQ_ideal, axis=0)
     _RESULT['A_CS_IDEAL'] = np.mean(np.abs(renyi_rhoA_CS - renyi_rhoA_ideal), axis=0)
     _RESULT['A_HAMMING_IDEAL'] = np.mean(np.abs(renyi_rhoA_hamming - renyi_rhoA_ideal), axis=0)
     _RESULT['AQ_CS_IDEAL'] = np.mean(np.abs(renyi_rhoAQ_CS - renyi_rhoAQ_ideal), axis=0)
     _RESULT['AQ_HAMMING_IDEAL'] = np.mean(np.abs(renyi_rhoAQ_hamming - renyi_rhoAQ_ideal), axis=0)
+
     _RESULT['A_CS_IDEAL_ERRORBAR'] = np.std(np.abs(renyi_rhoA_CS - renyi_rhoA_ideal), axis=0) / np.sqrt(epoches)
-    _RESULT['A_HAMMING_IDEAL_ERRORBAR'] = np.std(np.abs(renyi_rhoA_hamming - renyi_rhoA_ideal), axis=0) / np.sqrt(epoches)
+    _RESULT['A_HAMMING_IDEAL_ERRORBAR'] = np.std(np.abs(renyi_rhoA_hamming - renyi_rhoA_ideal), axis=0) / np.sqrt(
+        epoches)
     _RESULT['AQ_CS_IDEAL_ERRORBAR'] = np.std(np.abs(renyi_rhoAQ_CS - renyi_rhoAQ_ideal), axis=0) / np.sqrt(epoches)
-    _RESULT['AQ_HAMMING_IDEAL_ERRORBAR'] = np.std(np.abs(renyi_rhoAQ_hamming - renyi_rhoAQ_ideal), axis=0) / np.sqrt(epoches)
+    _RESULT['AQ_HAMMING_IDEAL_ERRORBAR'] = np.std(np.abs(renyi_rhoAQ_hamming - renyi_rhoAQ_ideal), axis=0) / np.sqrt(
+        epoches)
 
     return _RESULT
+
+
+def data_preparation_MK_error(MLIST, KLIST):
+    def process_Errors(file_name):
+        """
+        Errors with ErrorBar. This function does not distinguish the errors in THETAS and TIMES.
+        """
+        try:
+            with open(file_name, 'rb') as f:
+                data = pickle.load(f)
+
+            keys = ['A_CS_IDEAL', 'A_HAMMING_IDEAL', 'AQ_CS_IDEAL', 'AQ_HAMMING_IDEAL']
+            error_keys = [key + '_ERRORBAR' for key in keys]
+
+            means = [np.mean(data[key]) for key in keys]
+            errors = [np.mean(data[key]) for key in error_keys]
+
+            return means + errors
+        except (FileNotFoundError, KeyError) as e:
+            raise e
+
+    _RESULT = np.array([
+        [
+            process_Errors(f'QMpemba/QMpemba_M{_M}_K{_K}_N10_A[3, 4]_EP50.pkl')
+            for _K in KLIST
+        ]
+        for _M in MLIST
+    ])
+
+    return _RESULT
+
+
+def plot_Errors_Hamming_CS(MLIST, KLIST, Z, Zerror, save: bool = False, **kwargs):
+    x_indices, y_indices = np.arange(len(KLIST)), np.arange(len(MLIST))
+    fig, ax = plt.subplots(figsize=(8, 6), dpi=300)
+
+    cmap = 'coolwarm'
+    norm = Normalize(vmin=min(Z[0].min(), Z[1].min()), vmax=max(Z[0].max(), Z[1].max()))
+
+    for i in range(len(MLIST)):
+        for j in range(len(KLIST)):
+            top_triangle = Polygon(np.array([(x_indices[j] - 0.5, y_indices[i] - 0.5),
+                                             (x_indices[j] + 0.5, y_indices[i] - 0.5),
+                                             (x_indices[j], y_indices[i])]),
+                                   color=plt.get_cmap(cmap)(norm(Z[0][i, j])), ec='black')
+            ax.add_patch(top_triangle)
+
+            bottom_triangle = Polygon(np.array([(x_indices[j] - 0.5, y_indices[i] + 0.5),
+                                                (x_indices[j] + 0.5, y_indices[i] + 0.5),
+                                                (x_indices[j], y_indices[i])]),
+                                      color=plt.get_cmap(cmap)(norm(Z[1][i, j])), ec='black')
+            ax.add_patch(bottom_triangle)
+
+            ax.errorbar(x_indices[j], y_indices[i] - 0.25, yerr=Zerror[0][i, j], fmt='.', color='black', capsize=6)
+            ax.errorbar(x_indices[j], y_indices[i] + 0.25, yerr=Zerror[1][i, j], fmt='.', color='#FF7F0E', capsize=6)
+
+    sm = plt.cm.ScalarMappable(cmap=plt.get_cmap(cmap), norm=norm)
+    cbar = fig.colorbar(sm, ax=ax, label="Values", orientation='vertical', fraction=0.037, pad=0.04)
+    cbar.ax.tick_params(labelsize=10)
+
+    ax.set_aspect('equal', adjustable='box')
+    ax.set_xticks(x_indices)
+    ax.set_yticks(y_indices)
+    ax.set_xticklabels(KLIST, fontsize=16)
+    ax.set_yticklabels(MLIST, fontsize=16)
+
+    ax.set_xlabel(r'$K$', fontsize=20)
+    ax.set_ylabel(r'$M$', fontsize=20)
+
+    white_error_legend = mlines.Line2D([], [], color='black', marker='.', markersize=10, linestyle='None',
+                                       label='Classical Shadow')
+    black_error_legend = mlines.Line2D([], [], color='#FF7F0E', marker='.', markersize=10, linestyle='None',
+                                       label='Hamming')
+    ax.legend(handles=[white_error_legend, black_error_legend], loc='upper center', bbox_to_anchor=(0.83, -0.065),
+              ncol=2)
+
+    plt.title('Errors of two methods with errorbars', fontsize=20)
+    plt.subplots_adjust(left=0.15, right=0.85, bottom=0.2)
+    plt.tight_layout()
+
+    if save:
+        save_location = f"../figures/QMpembaErrors_N{kwargs.get('N')}_A{kwargs.get('subA')}.pdf"
+        plt.savefig(save_location)
 
 
 def plot_EA_Hamming_CS_THETAS(result, save: bool = False):
@@ -237,7 +332,8 @@ def plot_EA_Hamming_CS_THETAS(result, save: bool = False):
     for i, theta in enumerate(theta_list):
         plt.errorbar(time_list, aq_a_ideal[i], label=result["THETA_LABELS"][i], fmt='o', capsize=1, linestyle='-')
 
-        plt.errorbar(time_list, aq_a_hamming[i], yerr=aq_a_hamming_error[i], fmt='^', capsize=1, linestyle='--', alpha=0.5)
+        plt.errorbar(time_list, aq_a_hamming[i], yerr=aq_a_hamming_error[i], fmt='^', capsize=1, linestyle='--',
+                     alpha=0.5)
         plt.errorbar(time_list, aq_a_cs[i], yerr=aq_a_cs_error[i], fmt='*', capsize=1, linestyle='-.', alpha=0.5)
 
     _custom_lines = [
@@ -249,8 +345,9 @@ def plot_EA_Hamming_CS_THETAS(result, save: bool = False):
     plt.ylabel("$\\Delta S$", fontsize=20)
     plt.xticks(fontsize=18)
     plt.yticks(fontsize=18)
-    plt.title(f"EA for N{result.get('QNUMBER_TOTAL')}-A{result.get('SUBSYSTEM_A')}, M={result['M']}, K={result['K']}", fontsize=18)
-    plt.legend(handles=plt.gca().get_legend_handles_labels()[0] + _custom_lines, fontsize=14)
+    plt.title(f"EA for N{result.get('QNUMBER_TOTAL')}-A{result.get('SUBSYSTEM_A')}, M={result['M']}, K={result['K']}",
+              fontsize=18)
+    plt.legend(handles=plt.gca().get_legend_handles_labels()[0] + _custom_lines, fontsize=18)
     plt.tight_layout()
 
     if save:
@@ -259,41 +356,48 @@ def plot_EA_Hamming_CS_THETAS(result, save: bool = False):
 
 
 if __name__ == '__main__':
-    QNUMBER_TOTAL, SUBSYSTEM_A = 10, [3, 4]
-    INT_SLICES = 20
+    MLIST = [10, 50, 100, 200, 500, 1000]
+    KLIST = [50, 100, 200, 500, 1000]
 
-    ZZ_COEFFICIENT = 0
-    COUPLING_STRENGTH = 50      # MHz
+    for M in MLIST:
+        print(f'========== Executing M:{M} ==========')
+        for K in KLIST:
+            print(f'----- Executing K:{K} -----')
+            INT_SLICES = 20
+            QNUMBER_TOTAL, SUBSYSTEM_A = 10, [2, 3, 4]
 
-    TIME_MAX, TIME_SLICES = 50e-3, 50      # \mu s
+            ZZ_COEFFICIENT = 0
+            COUPLING_STRENGTH = 50  # MHz
 
-    M, K = 100, 100
-    EPOCHES = 50
+            TIME_MAX, TIME_SLICES = 50e-3, 50  # \mu s
 
-    THETA_LABELS = [r'$4/5$', r'$\pi/3$', r'$3/2$']
-    THETA_LIST = [4/5, np.pi/3, 3 / 2]
-    TIME_LIST = np.linspace(0, TIME_MAX, TIME_SLICES)
+            # M, K = 100, 100
+            EPOCHES = 50
 
-    print(f'QNUMBER_TOTAL: {QNUMBER_TOTAL}, SUBSYSTEM_A: {SUBSYSTEM_A},'
-          f' M: {M}, K: {K}, INT_SLICES: {INT_SLICES}, TIME_MAX: {TIME_MAX}, THETA_LABELS: {THETA_LABELS}')
+            THETA_LABELS = [r'$4/5$', r'$\pi/3$', r'$3/2$']
+            THETA_LIST = [4 / 5, np.pi / 3, 3 / 2]
+            TIME_LIST = np.linspace(0, TIME_MAX, TIME_SLICES)
 
-    model = SimpleModel(
-        QNUMBER_TOTAL, SUBSYSTEM_A, THETA_LIST,
-        coupling_strength=COUPLING_STRENGTH, zzCoefficient=ZZ_COEFFICIENT,
-        superposition=True, quench=True, timeList=TIME_LIST
-    )
+            print(f'QNUMBER_TOTAL: {QNUMBER_TOTAL}, SUBSYSTEM_A: {SUBSYSTEM_A},'
+                  f' M: {M}, K: {K}, INT_SLICES: {INT_SLICES}, TIME_MAX: {TIME_MAX}, THETA_LABELS: {THETA_LABELS}')
 
-    eaCalculator = EntanglementAsymmetry(QNUMBER_TOTAL, subA=SUBSYSTEM_A, L=INT_SLICES, Q=model.Q)
+            model = SimpleModel(
+                QNUMBER_TOTAL, SUBSYSTEM_A, THETA_LIST,
+                coupling_strength=COUPLING_STRENGTH, zzCoefficient=ZZ_COEFFICIENT,
+                superposition=True, quench=True, timeList=TIME_LIST
+            )
 
-    results = eaCalculator.theta_timeRun(model.rhos_with_time_ferroSuperpositionRho, M, K, epoches=EPOCHES)
-    format_results = data_preparation(results=results,
-                                      M=M, K=K, INT_SLICES=INT_SLICES,
-                                      THETA_LIST=THETA_LIST, THETA_LABELS=THETA_LABELS,
-                                      QNUMBER_TOTAL=QNUMBER_TOTAL, SUBSYSTEM_A=SUBSYSTEM_A,
-                                      TIME_SLICES=TIME_SLICES, TIME_MAX=TIME_MAX, TIME_LIST=TIME_LIST,
-                                      COUPLING_STRENGTH=COUPLING_STRENGTH, EPOCHES=EPOCHES)
+            eaCalculator = EntanglementAsymmetry(QNUMBER_TOTAL, subA=SUBSYSTEM_A, L=INT_SLICES, Q=model.Q)
 
-    with open(f'../data/QMpemba_M{M}_K{K}_N{QNUMBER_TOTAL}_A{SUBSYSTEM_A}.pkl', 'wb') as f:
-        pickle.dump(format_results, f)
+            results = eaCalculator.theta_timeRun(model.rhos_with_time_ferroSuperpositionRho, M, K, epoches=EPOCHES)
+            format_results = data_preparation_EA(results=results,
+                                                 M=M, K=K, INT_SLICES=INT_SLICES,
+                                                 THETA_LIST=THETA_LIST, THETA_LABELS=THETA_LABELS,
+                                                 QNUMBER_TOTAL=QNUMBER_TOTAL, SUBSYSTEM_A=SUBSYSTEM_A,
+                                                 TIME_SLICES=TIME_SLICES, TIME_MAX=TIME_MAX, TIME_LIST=TIME_LIST,
+                                                 COUPLING_STRENGTH=COUPLING_STRENGTH, EPOCHES=EPOCHES)
 
-    plot_EA_Hamming_CS_THETAS(format_results, save=True)
+            with open(f'../data/QMpemba/QMpemba_M{M}_K{K}_N{QNUMBER_TOTAL}_A{SUBSYSTEM_A}_EP{EPOCHES}.pkl', 'wb') as f:
+                pickle.dump(format_results, f)
+
+            # plot_EA_Hamming_CS_THETAS(format_results, save=True)
